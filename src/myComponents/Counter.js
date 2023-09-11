231404aa0045da156f9cae058d421a0197216c2c
FatemehNilchi <fatemehnilchi77@gmail.com>
2023-09-11 18:30:52 +0330

    simple counter

---
+++ src/myComponents/Counter.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+
+export default function Counter() {
+    const [count , setCount]= useState(0);
+  return (
+   <div>
+    <button onClick={()=>setCount(count +1)}>+</button>
+     <div>{count}</div>
+     <button onClick={()=>setCount(count -1)}>-</button>
+     </div>
+  )
+}
